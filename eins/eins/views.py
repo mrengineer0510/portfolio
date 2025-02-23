66bcd2c7ef6ba11101bb695c2a517ae15112dc88
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-23 15:51:23 +0530

    initiral commits

---
+++ eins/eins/views.py
@@ -0,0 +1,22 @@
+#create on Feb 21
+import markdown
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepg(request):
+    return render(request, "index.html")
+def menu(request):
+    return render(request, "menu.html")
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def projects(request):
+    return render (request, "projects.html")
+
+def inf(request):
+    return render(request, "info.md")
