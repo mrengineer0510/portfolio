0f9ea13b77231bdd149c75d6a46ffcc8e72d5027
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-24 15:03:04 +0530

    Update views.py

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,1 +1,21 @@
 
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepg(request):
+    return render(request, "index.html")
+def menu(request):
+    return render(request, "menu.html")
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def projects(request):
+    return render (request, "projects.html")
+
+def inf(request):
+    return render(request, "info.md")
